1cb530ec4b23ff3d7844c63abe24f387536c296f
Sheila <sheilalopes.automacao@gmail.com>
2023-06-13 17:52:59 -0300

    Incluida assertivas para o body do teste incluirPet

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -10,6 +10,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
 
 // 3 - Classe
 public class Pet {
@@ -40,6 +41,8 @@ public class Pet {
         .then() // Então
                 .log().all()
                 .statusCode(200)
+                .body("name", is("Taliba"))
+                .body("status", is("available"))
         ;
 
 
